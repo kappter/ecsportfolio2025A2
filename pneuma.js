6f3ca3c63c459aafe3f37f3f2cc8e5e2dbca1526
Ken Kapptie <kkapptie@graniteschools.org>
2025-03-25 10:40:17 -0600

    Update pneuma.js

--- pneuma.js
+++ pneuma.js
@@ -6,90 +6,100 @@ const pneumaSong = {
   blocks: [
     {
       type: "intro",
-      measures: 36,
+      measures: 16,
       rootNote: "D",
       mode: "Dorian",
-      tempo: 124,
+      tempo: 120,
       timeSignature: "7/4",
       feel: "Atmospheric",
-      lyrics: "" // Extended intro riff
+      lyrics: "" // Extended atmospheric riff
     },
     {
       type: "verse",
-      measures: 36,
+      measures: 16,
       rootNote: "D",
       mode: "Dorian",
-      tempo: 124,
+      tempo: 120,
       timeSignature: "7/4",
       feel: "Mystical",
       lyrics: "We are spirit bound to this flesh, we go round one foot nailed down"
     },
     {
       type: "interlude",
-      measures: 36,
+      measures: 16,
       rootNote: "D",
       mode: "Dorian",
-      tempo: 124,
+      tempo: 120,
       timeSignature: "7/4",
       feel: "Trippy",
-      lyrics: "" // Extended drumming
+      lyrics: "" // Middle Eastern drumming
     },
     {
       type: "verse",
-      measures: 36,
+      measures: 16,
       rootNote: "D",
       mode: "Dorian",
-      tempo: 124,
+      tempo: 120,
       timeSignature: "7/4",
       feel: "Mystical",
       lyrics: "But bound to reach out and beyond this flesh, become Pneuma"
     },
     {
       type: "chorus",
-      measures: 16,
+      measures: 12,
       rootNote: "D",
       mode: "Dorian",
-      tempo: 124,
+      tempo: 120,
       timeSignature: "4/4",
       feel: "Awakening",
       lyrics: "Child, wake up, child, release the light"
     },
     {
       type: "interlude",
-      measures: 36,
+      measures: 16,
       rootNote: "D",
       mode: "Dorian",
-      tempo: 124,
+      tempo: 120,
       timeSignature: "7/4",
       feel: "Intense",
-      lyrics: "" // Extended build
+      lyrics: "" // Instrumental build
     },
     {
       type: "bridge",
-      measures: 36,
+      measures: 24,
       rootNote: "D",
       mode: "Dorian",
-      tempo: 124,
+      tempo: 120,
       timeSignature: "7/4",
       feel: "Tension",
-      lyrics: "" // Polyrhythmic section
+      lyrics: "" // Heavier riff section
+    },
+    {
+      type: "chorus",
+      measures: 12,
+      rootNote: "D",
+      mode: "Dorian",
+      tempo: 120,
+      timeSignature: "4/4",
+      feel: "Awakening",
+      lyrics: "Wake up, remember, we are born of one breath"
     },
     {
       type: "bridge",
-      measures: 36,
+      measures: 32,
       rootNote: "D",
       mode: "Dorian",
-      tempo: 124,
+      tempo: 120,
       timeSignature: "7/4",
       feel: "Climactic",
-      lyrics: "" // Extended solo
+      lyrics: "" // Extended climactic instrumental
     },
     {
       type: "outro",
-      measures: 82,
+      measures: 16,
       rootNote: "D",
       mode: "Dorian",
-      tempo: 124,
+      tempo: 120,
       timeSignature: "7/4",
       feel: "Resolution",
       lyrics: "Pneuma, reach out and beyond, wake up remember"
@@ -97,8 +107,10 @@ const pneumaSong = {
   ]
 };
 
-// Ensure global access
 window.loadPneuma = function() {
   console.log("Executing loadPneuma...");
   loadSongData(pneumaSong);
 };
+
+// Optional: Auto-load for testing
+// window.loadPneuma();
